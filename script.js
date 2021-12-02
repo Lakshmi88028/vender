15fa26780b53c20eb6e256bf52d36304f6cdabda
Lakshmi88028 <89910790+Lakshmi88028@users.noreply.github.com>
2021-12-02 21:51:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+function printChats( name){
+        
+    if(name=="panipuri"){
+        var rate=30;
+        var quantity=document.getElementById("name1").value;
+        var total=quantity*rate;
+        document.getElementById("print1").innerHTML=total;
+       
+    }
+
+    if(name=="FrenchFries"){
+        var rate=60;
+        var quantity=document.getElementById("name2").value;
+        var total=quantity*rate;
+        document.getElementById("print2").innerHTML=total;
+       
+    }
+
+    if(name=="Gobi"){
+       var rate=50;
+        var quantity=document.getElementById("name3").value;
+        var total=quantity*rate;
+        document.getElementById("print3").innerHTML=total;
+       
+    }
+
+    if(name=="samosa"){
+        var rate=30;
+        var quantity=document.getElementById("name4").value;
+        var total=quantity*rate;
+        document.getElementById("print4").innerHTML=total;
+       
+    }
+
+    if(name=="kachori"){
+        var rate=30;
+        var quantity=document.getElementById("name5").value;
+        var total=quantity*rate;
+        document.getElementById("print5").innerHTML=total;
+       
+    }
+    
+    if(name=="Noodles"){
+        var rate=70;
+        var quantity=document.getElementById("name6").value;
+        var total=quantity*rate;
+        document.getElementById("print6").innerHTML=total;
+       
+    }
+    if(name=="vadapav"){
+        var rate=20;
+        var quantity=document.getElementById("name7").value;
+        var total=quantity*rate;
+        document.getElementById("print7").innerHTML=total;
+       
+    }
+    if(name=="Masalapuri"){
+        var rate=30;
+        var quantity=document.getElementById("name8").value;
+        var total=quantity*rate;
+        document.getElementById("print8").innerHTML=total;
+       
+    }
+    if(name=="cone"){
+        var rate=50;
+        var quantity=document.getElementById("name9").value;
+        var total=quantity*rate;
+        document.getElementById("print9").innerHTML=total;
+       
+    }
+    if(name=="chocobar"){
+        var rate=50;
+        var quantity=document.getElementById("name10").value;
+        var total=quantity*rate;
+        document.getElementById("print10").innerHTML=total;
+       
+    }
+    if(name=="classic"){
+        var rate=80;
+        var quantity=document.getElementById("name11").value;
+        var total=quantity*rate;
+        document.getElementById("print11").innerHTML=total;
+       
+    }
+
+    if(name=="mangodolly"){
+        var rate=45;
+        var quantity=document.getElementById("name12").value;
+        var total=quantity*rate;
+        document.getElementById("print12").innerHTML=total;
+       
+    }
+    if(name=="cupIce"){
+        var rate=45;
+        var quantity=document.getElementById("name13").value;
+        var total=quantity*rate;
+        document.getElementById("print13").innerHTML=total;
+       
+    }
+
+    if(name=="ButterPecan"){
+        var rate=80;
+        var quantity=document.getElementById("name14").value;
+        var total=quantity*rate;
+        document.getElementById("print14").innerHTML=total;
+       
+    }
+    if(name=="Neapolitan"){
+        var rate=60;
+        var quantity=document.getElementById("name15").value;
+        var total=quantity*rate;
+        document.getElementById("print15").innerHTML=total;
+       
+    }
+    if(name=="PralinePecan"){
+        var rate=80;
+        var quantity=document.getElementById("name16").value;
+        var total=quantity*rate;
+        document.getElementById("print16").innerHTML=total;
+       
+    }
+    if(name=="vanilla"){
+        var rate=40;
+        var quantity=document.getElementById("name17").value;
+        var total=quantity*rate;
+        document.getElementById("print17").innerHTML=total;
+       
+    }
+
+    if(name=="vanillaFudgeRipple"){
+        var rate=90;
+        var quantity=document.getElementById("name17").value;
+        var total=quantity*rate;
+        document.getElementById("print17").innerHTML=total;
+       
+    }
+   
+    
+}
